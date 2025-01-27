009526266c867816ea211af945ad02470792f626
satyam <satyamb971@gmail.com>
2025-01-27 14:24:12 +0530

    update social link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 import { useEffect, useState } from "react";
 import Typewriter from 'typewriter-effect';
-
+import linkedIn from "../assets/social/linkein.png";
+import github from "../assets/social/git.png";
 const Hero = () => {
 
   return (
@@ -18,44 +18,52 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-col items-start">
-  <motion.h1
-    className="text-4xl md:text-6xl font-extrabold text-white mb-6 leading-tight"
-    initial={{ opacity: 0, y: -20 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ type: "spring", stiffness: 200, damping: 12 }}
-  >
-    Hi, I'm{" "}
-    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
-      Satyam <span className="animate-waving-hand">👋</span>
-    </span>
-  </motion.h1>
+          <motion.h1
+            className="text-4xl md:text-6xl font-extrabold text-white mb-6 leading-tight"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ type: "spring", stiffness: 200, damping: 12 }}
+          >
+            Hi, I'm{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
+              Satyam <span className="animate-waving-hand">👋</span>
+            </span>
+          </motion.h1>
 
-  <motion.p
-    className="mt-4 text-xl sm:text-2xl text-white font-medium bg-gradient-to-r from-gray-200 via-gray-300 to-gray-400 bg-clip-text text-transparent"
-    initial={{ opacity: 0, y: 20 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ delay: 0.3, type: "spring", stiffness: 200, damping: 12 }}
-  >
-    <Typewriter
-      options={{
-        strings: [
-          "Frontend Developer",
-          "Backend Developer",
-          "MERN Stack Developer",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 1000,
-      }}
-    />
-  </motion.p>
-</div>
-
+          <motion.p
+            className="mt-4 text-xl sm:text-2xl text-white font-medium bg-gradient-to-r from-gray-200 via-gray-300 to-gray-400 bg-clip-text text-transparent"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.3, type: "spring", stiffness: 200, damping: 12 }}
+          >
+            <Typewriter
+              options={{
+                strings: [
+                  "Frontend Developer",
+                  "Backend Developer",
+                  "MERN Stack Developer",
+                ],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 50,
+                typeSpeed: 100,
+                backSpeed: 50,
+                backDelay: 1000,
+              }}
+            />
+          </motion.p>
 
 
+          <div className="flex space-x-6 mt-4">
+            <a href="www.linkedin.com/in/satyam-manojlal-bharti-13997b214" target="_blank" rel="noopener noreferrer">
+              <img src={linkedIn} alt="LinkedIn" className="w-10 h-10 transition-transform duration-300 hover:scale-110" />
+            </a>
+            <a href="https://github.com/satyam70288" target="_blank" rel="noopener noreferrer">
+              <img src={github} alt="GitHub" className=" bg-white-100 rounded-full w-10 h-10 transition-transform duration-300 hover:scale-110 text-white-100" />
+            </a>
+          </div>
+
+        </div>
       </div>
 
       <ComputersCanvas />
